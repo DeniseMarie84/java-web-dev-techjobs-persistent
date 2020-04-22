5d3867e5851b917cd294d884aa654adbc8f7827a
Denise Williams <denise.marie.84@gmail.com>
2020-04-21 23:22:27 -0500

    part 3 finished post cleaning class errors

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,7 +1,11 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -9,8 +13,10 @@ public class Skill extends AbstractEntity {
     @Size(max=255)
     private String description;
 
-    public Skill(){}
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
 
+    public Skill(){}
 
     public String getDescription() {
         return description;
@@ -20,5 +26,8 @@ public class Skill extends AbstractEntity {
         this.description = description;
     }
 
+    public List<Job> getJobs() { return jobs; }
+
+    public void setJobs(List<Job> jobs) { this.jobs = jobs;}
 
 }
